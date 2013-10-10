412f96a6a4579d23f3858443df2b1a5f1a8bc184
Kevin Traver <kevintraver@gmail.com>
2013-10-09 20:35:31 -0700

    remove unneeded migrations

--- db/migrate/20130806133347_add_number_of_seats_to_rides.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberOfSeatsToRides < ActiveRecord::Migration
-  def change
-    add_column :rides, :number_of_seats, :integer
-  end
-end
--- db/migrate/20130806133557_add_ride_number_of_seats_to_requests.rb
+++
@@ -1,5 +0,0 @@
-class AddRideNumberOfSeatsToRequests < ActiveRecord::Migration
-  def change
-    add_column :requests, :ride_number_of_seats, :integer
-  end
-end
